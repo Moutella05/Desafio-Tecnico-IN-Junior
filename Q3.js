dfaedd7e98009d62359b7f4849d981a9db6d4b32
Joao Vitor Moutella Lopes <Jvmoutella@gmail.com>
2025-06-28 13:40:45 -0300

    Q3 completa

---
+++ Q3.js
@@ -0,0 +1,24 @@
+let idade =1
+let i
+let pessoas = []
+while(idade>0){
+    idade = Number(prompt(`Digite sua idade`))
+    pessoas.push(idade)
+}
+let verificar = verificarIdade(pessoas)
+for(i=0; i<verificar.length; i++){
+    console.log(`A pessoa ${i+1} e : ${verificar[i]}`)
+}
+
+function verificarIdade(pessoas){
+    let maiorIdade = []
+    for(i=0; i<pessoas.length; i++){
+        if(pessoas[i] >= 18){
+            maiorIdade.push(`Maior de idade`)
+        }
+        else{
+            maiorIdade.push(`Menor de idade`)
+        }
+    }
+    return maiorIdade
+}
