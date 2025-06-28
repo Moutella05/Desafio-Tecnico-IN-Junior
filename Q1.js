fd2097e43bb8db219da8b444d6ee954460cc9ace
Joao Vitor Moutella Lopes <Jvmoutella@gmail.com>
2025-06-28 11:57:17 -0300

    Adicionei o calculo das raizes e os resultados

--- Q1.js
+++ Q1.js
@@ -1,3 +1,17 @@
-let a = parseInt(prompt('Digite o valor de A'))
+let a = parseInt(prompt(`Digite o valor de A`))
 let b = parseInt(prompt('Digite o valor de B'))
 let c = parseInt(prompt('Digite o valor de C'))
+
+let delta = (b*b)-4*a*c
+r1 = (-b+Math.sqrt(delta)) / (2*a)
+r2 = (-b-Math.sqrt(delta)) / (2*a)
+
+if (delta>0){
+    console.log(`Raiz 1: ${r1} Raiz 2: ${r2}`)
+}
+else if (r1===r2){
+    console.log(`Raizez iguais: ${r1}`)
+}
+else{
+    console.log(`Nao possui raizes reais`)
+}
