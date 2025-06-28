c5f05f461c2f2b61d537d8c7aa82794451b9412c
Joao Vitor Moutella Lopes <Jvmoutella@gmail.com>
2025-06-28 14:12:32 -0300

    Q4 completa

---
+++ Q4.js
@@ -0,0 +1,6 @@
+reais = Number(prompt(`Digite o valor em reais:`))
+dolar = reais/5.70
+euro = reais/6.10
+console.log(`Valor em reais: ${reais}`)
+console.log(`Valor em dolar: ${dolar}`)
+console.log(`Valor em euro: ${euro}`)
